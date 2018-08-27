23e24f491681fece67f7cd8a0066941be0b3598a
Jimmy Truong <thamestruong@gmail.com>
2018-08-26 21:58:47 -0700

    holy shit got backend to connect with the frontend

---
+++ app/javascript/common/utils/token.ts
@@ -0,0 +1,2 @@
+import Cookies from "js-cookie";
+export default () => Cookies.get("customer_token");
---
+++ app/javascript/numbers/Application.tsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { ApolloProvider, Query } from "react-apollo";
+import client from "../common/graphql/client";
+import { map } from "lodash";
+
+// graphql specific
+import gql from "graphql-tag";
+
+const GET_ALL_LINKS = gql`
+  query {
+    allLinks {
+      id
+      description
+      url
+    }
+  }
+`;
+
+class Application extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Query query={GET_ALL_LINKS}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <div> loading ... </div>;
+            }
+
+            if (error) {
+              return <div> error ... </div>;
+            }
+
+            if (!data) {
+              return <div> no data ...</div>;
+            }
+
+            return (
+              <div>
+                {map(data.allLinks, link => (
+                  <div> {link.url} </div>
+                ))}
+              </div>
+            );
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default Application;
